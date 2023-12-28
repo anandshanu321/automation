d428585057c84f0c6eeb10affa4134940ff5ddeb
hp <hp@LAPTOP-PT5FP9QT>
2023-12-28 19:31:47 +0530

    dfghjj

---
+++ automation2/src/automation/home.java
@@ -0,0 +1,9 @@
+package automation;
+
+public class home {
+
+	public static void main(String[] args) {
+     System.out.println("abgfd");
+	}
+
+}
--- automation2/src/automation/login.java
+++ automation2/src/automation/login.java
@@ -4,6 +4,7 @@ public class login {
 
 	public static void main(String[] args) {
          System.out.println("shanu");
+         System.out.println("abvgf");
 	}
 
 }
